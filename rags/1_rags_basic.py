0534de064ea47a77adb6e74de48177793cd62581
anjali <shyanj05@gmail.com>
2025-07-31 18:06:19 +0530

    Creating basic implemenation of rags for qubo data

--- rags/1_rags_basic.py
+++ rags/1_rags_basic.py
@@ -1,3 +1,52 @@
+from langchain_community.embeddings import HuggingFaceEmbeddings,OpenAIEmbeddings
+from langchain_community.document_loaders import TextLoader
+from langchain_community.vectorstores import FAISS
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_openai import OpenAI
+from langchain_core.messages import SystemMessage, HumanMessage
+from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
+load_dotenv()
 cur_dir=os.path.dirname(os.path.abspath(__file__))
-print(cur_dir)
+# print(cur_dir)
+extracted_cards_path= os.path.join(cur_dir,'bot_data', 'extracted_cards.txt')
+cards_path= os.path.join(cur_dir,'bot_data', 'cards.txt')
+links_path=os.path.join(cur_dir,'bot_data', 'links_in_bot.txt')
+persistent_path=os.path.join(cur_dir,'vectors', 'chroma_db')
+#loading the data for chunks
+loader= TextLoader(links_path,encoding="utf-8")
+documents=loader.load()
+# RecursiveCharacterTextSplitter
+splitter=CharacterTextSplitter(chunk_size=1000,chunk_overlap=100)
+splitter.split_documents(documents)
+
+# embedding_model=HuggingFaceEmbeddings(
+embedding_model=HuggingFaceEmbeddings(
+    model_name="all-MiniLM-L6-v2"
+)  
+vector_store=FAISS.from_documents(documents,embedding_model)
+# vector_store.save_local(db_dir)
+
+query="qsg for bike cam"
+
+
+similar_chunks=vector_store.similarity_search(query,k=1)
+
+# for i, doc in enumerate(similar_chunks):
+#     pa
+#     print(f"document {i}\n {doc.page_content}\n{doc.metadata}\n")
+
+input=(
+    "Here are some of the documents that can help you to answer the question- "+
+    query
+    +"relevant documents are\n"
+    +"\n\n".join([doc.page_content for doc in similar_chunks])
+    +"please provide answer based on the document provided only"
+    )
+model=OpenAI(model="gpt-4o-mini")
+message=[SystemMessage(content="You are a qubo assistant and are here to resolve issues related to qubo devices"),
+         HumanMessage(content=input)]
+response=model.invoke(message)
+print("response:", response)
+
--- rags/code_to_extract_bot_data.py
+++ rags/code_to_extract_bot_data.py
@@ -18,9 +18,7 @@ extracted_cards_path= os.path.join(cur_dir,'bot_data', 'extracted_cards.txt')
 # # getting all cards data from the file code ende here
 
 # # gets the extracted cards data from the cards file
-
 # docs = []
-# # print(cards_data)
 # with open(cards_path, "r", encoding="utf-8") as f:
 #     cards = json.load(f) 
 #     cards_data = cards.get("en", {})      
@@ -40,7 +38,7 @@ extracted_cards_path= os.path.join(cur_dir,'bot_data', 'extracted_cards.txt')
 #               "link":link
 #             }
 #         else:
-#             data = {"message": card_name}
+#             data = {"message": message}
 #     suggestions = rendering.get("suggestions", [])
 #     options=[]
 #     for suggestion in suggestions:
@@ -58,24 +56,68 @@ extracted_cards_path= os.path.join(cur_dir,'bot_data', 'extracted_cards.txt')
 #         file.write(str(item) + "\n")
 # # Save the extracted cards data to a file and code for same ends here
 
-# getting all links
-links_docs=[]
-with open(cards_path,"r",encoding="utf-8") as file:
-    cards_data=json.load(file)
-    cards_data = cards_data.get("en", {})  
-    for card_id, card in cards_data.items():
-        card_name = card.get("name", "Unnamed Card")
-        rendering = card.get("rendering_config", {})
-        replies=rendering.get("replies",[])
-        for reply in replies:
-            links=reply.get("links")
-            if(links):
-                links_docs.append({card_name:links[0].get("url")})
-with open(os.path.join(cur_dir,"bot_data","links_in_bot.txt"),"w", encoding="utf-8")as file:
-    for link in links_docs:
-        file.write(str(link)+"\n")
+# # getting all links
+# links_docs=[]
+# with open(cards_path,"r",encoding="utf-8") as file:
+#     cards_data=json.load(file)
+#     cards_data = cards_data.get("en", {})  
+#     for card_id, card in cards_data.items():
+#         card_name = card.get("name", "Unnamed Card")
+#         rendering = card.get("rendering_config", {})
+#         replies=rendering.get("replies",[])
+#         for reply in replies:
+#             links=reply.get("links")
+#             if(links):
+#                 links_docs.append({card_name:links[0].get("url")})
+# with open(os.path.join(cur_dir,"bot_data","links_in_bot.txt"),"w", encoding="utf-8")as file:
+#     for link in links_docs:
+#         file.write(str(link)+"\n")
+# # getting links code ends here
 
+with open(cards_path, "r", encoding="utf-8") as file:
+    data = json.load(file)
+    cards = data.get("en", {}) 
+    next_data= data.get("en", {}) 
+qa_pairs = []
+flow_summary = []
 
+# Traverse cards
+for card_id,card in cards.items():
+    # card_id = card.get("id")
+    card_name = card.get("name", "No Title")
+    rendering=card.get("rendering_config",{})
+    content = rendering.get("replies", [])
+    # print(card.get("criteria").get("default"))
+    next_action_id=card.get("criteria",{}).get("default",{}).get("next_action_id","")
+    
+    
+    # Extract message content
+    message = ""
+    for elem in content:
+       flow_summary.append(elem)
+    next_card=next_data.get(next_action_id)
+    flow_summary.append(next_card)
+    # buttons = []
+    # for elem in content:
+    #     if elem.get("type") == "button":
+    #         buttons.append((elem.get("text", ""), elem.get("targetCardId", "")))
 
+    # Create Q&A
+    qa_pairs.append({
+        "Question": card_name,
+        "Answer": flow_summary,
+    })
 
+    # Build flow
+    # flow_line = f"📌 **{title}** → {message}"
+    # if buttons:
+    #     flow_line += "\n➡️ Options: " + ", ".join([f"'{b[0]}' ➡ {card_map[b[1]]['title'] if b[1] in card_map else 'Unknown'}" for b in buttons])
+    # flow_summary.append(flow_line)
 
+# for qa in qa_pairs:
+#     print(f"Ques: {qa['Question']}")
+#     print(f"Ans: {qa['Answer']}")
+
+with open(os.path.join(cur_dir,"bot_data","QA.txt"),"w", encoding="utf-8")as file:
+    for line in flow_summary:
+        file.write(str(line)+"\n")
